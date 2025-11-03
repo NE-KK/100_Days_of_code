d314dd8a249c6d3f4f9319cd8bd8da2003e7675f
Nils Eckmann <nils.eckmann@mein.gmx>
2025-11-03 15:36:52 +0100

    day 15 with issues to fix

--- day_015/main.py
+++ day_015/main.py
@@ -2,7 +2,6 @@
 # Day 15 - Coffee machine
 
 
-
 MENU = {
     "espresso": {
         "ingredients": {
@@ -47,7 +46,26 @@ def print_resources():
     print(f"{bank} : ${bank['money']}")
 
 
+def check_resources(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
+
+def process_coins():
+    print("Please insert coins.")
+    total = 0
+    for coin in [0.25, 0.10, 0.05, 1.00]:
+        num_coins = int(input(f"How many {coin} coins?: "))
+        total += num_coins * coin
+    return total
+
+
 def main():
+    print(MENU)
+    print('latte' in MENU)
     machine_is_on = True
 
     while machine_is_on:
@@ -56,10 +74,31 @@ def main():
         if user_input == "off":
             machine_is_on = False
 
+        if user_input == "report":
+            print_resources()
+
+        if user_input in MENU:
+            resources_sufficient = check_resources(MENU[user_input]["ingredients"])
+            money_received = 0
+            
+            if resources_sufficient:
+                print("Please insert coins.")
+                money_received = process_coins()
+            else:
+                print("Sorry, there is not enough resources.")
+
+            if money_received >= MENU[user_input]["cost"]:
+                change = round(money_received - MENU[user_input]["cost"], 2)
+                print(f"Here is ${change} in change.")
+                bank["money"] += MENU[user_input]["cost"]
+
+                for item in MENU[user_input]["ingredients"]:
+                    resources[item] -= MENU[user_input]["ingredients"][item]
+
+                print(f"Here is your {user_input}. Enjoy!")
+            else:
+                print("Sorry that's not enough money. Money refunded.")
 
 
 if __name__ == "__main__":
     main()
-
-
-print_resources()
