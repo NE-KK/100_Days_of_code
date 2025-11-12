f18b00b5a562e9e88301ceb704545c49f3aeaa49
NE-KK <134157533+NE-KK@users.noreply.github.com>
2025-11-12 14:52:31 +0100

    day_016 finished

--- day_016/main.py
+++ day_016/main.py
@@ -1,3 +1,29 @@
+# 100 Days of code by Angela Yu
+# Day 16 - Coffee machine OOP version
+
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+print("Welcome to the Coffee Machine!")
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if drink and coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
