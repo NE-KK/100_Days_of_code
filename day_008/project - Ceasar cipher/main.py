5e1e086f87eb9d021c3bdc17f767f4f42af050f1
Nils Eckmann <nils.eckmann@mein.gmx>
2025-11-17 10:00:44 +0100

    day 008 description

--- day_008/project - Ceasar cipher/main.py
+++ day_008/project - Ceasar cipher/main.py
@@ -1,5 +1,5 @@
 # 100 Days of code by Angela Yu
-# Day 8 - Ceasar Cipher
+# Day 8 - Project - Ceasar Cipher
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
