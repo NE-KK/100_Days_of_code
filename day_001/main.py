694f3553e89472ebcf7e2e50cc68b1db2ca11b6e
NE-KK <134157533+NE-KK@users.noreply.github.com>
2025-12-04 13:34:02 +0100

    day001 GUI for Band name generator

--- day_001/main.py
+++ day_001/main.py
@@ -1,8 +1,42 @@
 # 100 Days of code by Angela Yu
 # Day 1 - Band Name Generator
 
-city = input("Name of the city where you grow up: ")
-pet = input("Name of your childhood pet: ")
+from tkinter import *
+from tkinter import ttk
 
 
-print(f"Your Band name is: {city} {pet}")
+def generate_band_name():
+    city_name = city_entry.get()
+    pet_name = pet_entry.get()
+    band_name = city_name + " " + pet_name
+
+    window_band_name = Tk()
+    window_band_name.title("Band name")
+    window_band_name.geometry("250x50")
+
+    ttk.Label(window_band_name, text="The Band name is:").pack()
+    ttk.Label(window_band_name, text=band_name).pack()
+
+
+root = Tk()
+root.title("Band Name Generator")
+root.geometry("300x180")
+
+frame_city = ttk.Frame(root, padding=10)
+frame_city.pack()
+frame_pet = ttk.Frame(root, padding=10)
+frame_pet.pack()
+frame_button = ttk.Frame(root, padding=10)
+frame_button.pack()
+
+ttk.Label(frame_city, text="City where you grew up.").pack()
+city_entry = ttk.Entry(frame_city)
+city_entry.pack()
+
+ttk.Label(frame_pet, text="Name of your childhood pet.").pack()
+pet_entry = ttk.Entry(frame_pet)
+pet_entry.pack()
+
+ttk.Button(frame_button, text="Generate", command=generate_band_name).pack()
+
+root.mainloop()
