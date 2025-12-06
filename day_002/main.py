29e9c72a2b6170fd3057540d5955caae65dcf189
NE-KK <134157533+NE-KK@users.noreply.github.com>
2025-12-07 00:29:15 +0100

    day 002 GUI minor change

--- day_002/main.py
+++ day_002/main.py
@@ -41,17 +41,3 @@ ttk.Button(root, text="Calculate", command=calculate_bill).pack()
 
 
 root.mainloop()
-
-
-
-"""
-print("Welcome to the tip calculator!")
-bill_amount = float(input("What was the bill? $"))
-tip_percent = int(input("How much tip would you like to give? 10, 12 or 15? "))
-group_size = int(input("How many people to split the bill? "))
-
-bill_total = bill_amount + (bill_amount * (tip_percent / 100))
-bill_share = round(bill_total / group_size, 2)
-
-print(f"Each person should pay: ${bill_share:.2f}")
-"""
