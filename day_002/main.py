4b057f67fe148da3b7c999825db08c845cbbaa48
NE-KK <134157533+NE-KK@users.noreply.github.com>
2025-12-07 01:57:25 +0100

    day 002 entry for number of people sharing the bill

--- day_002/main.py
+++ day_002/main.py
@@ -6,16 +6,20 @@ from tkinter import ttk
 
 
 def calculate_bill():
-    bill = int(bill_entry.get())
+    bill = float(bill_entry.get())
     percent = int(tip_entry.get()) / 100
+    bill_share = int(share_entry.get())
     final_bill = bill + (bill * percent)
+    final_price_per_person = final_bill / bill_share
 
     bill_result = Tk()
     bill_result.title("Result")
     bill_result.geometry("300x160")
 
-    result_label = ttk.Label(bill_result, text=f"The Total bill is: {final_bill:.2f}")
+    result_label = ttk.Label(bill_result, text=f"The Total bill is: ${final_bill:.2f}")
     result_label.pack()
+    per_person_label = ttk.Label(bill_result, text=f"Each person should pay: ${final_price_per_person:.2f}")
+    per_person_label.pack()
 
     bill_result.mainloop()
 
@@ -24,10 +28,10 @@ root = Tk()
 root.title("Tip Calculator")
 root.geometry("300x160")
 
-frame_bill = ttk.Frame(root, padding=10)
-frame_bill.pack()
-frame_tip = ttk.Frame(root, padding=10)
-frame_tip.pack()
+frame_bill = ttk.Frame(root, padding=10).pack()
+frame_tip = ttk.Frame(root, padding=10).pack()
+frame_share = ttk.Frame(root, padding=10).pack()
+
 
 ttk.Label(frame_bill, text="What was the bill? in $").pack()
 bill_entry = ttk.Entry(frame_bill)
@@ -37,7 +41,10 @@ ttk.Label(frame_tip, text="How much do you tip? in %").pack()
 tip_entry = ttk.Entry(frame_tip)
 tip_entry.pack()
 
-ttk.Button(root, text="Calculate", command=calculate_bill).pack()
+ttk.Label(frame_share, text="How many people share the bill?").pack()
+share_entry = ttk.Entry(frame_share)
+share_entry.pack()
 
+ttk.Button(root, text="Calculate", command=calculate_bill).pack()
 
 root.mainloop()
